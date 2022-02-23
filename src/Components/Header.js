7585f73dd2d6a9cd6026521600cccbe0a0deaba0
Jeevan19 <jeevanmca.ppm@gmail.com>
2022-02-23 20:47:40 +0530

    Header design

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,39 @@
 import React from 'react';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header class="bg-dark">
+      <div class="clearfix display-flex container" data-items="center">
+        <div class="logo">
+          <a href="">
+            <img
+              src="https://alvimurtaza.github.io/Interview-Front-end/images/l3-l4-engineer/logo.png"
+              alt="logo Placeholder"
+            />
+          </a>{' '}
+        </div>
+        <div class="align-items-center margin-left-auto">
+          <nav>
+            <ul class="nav">
+              <li class=" nav-link">
+                <a href="">Dashboard</a>
+              </li>
+              <li class="nav-link">
+                <a href="">Events</a>
+              </li>
+              <li class=" nav-link">
+                <a href="">Help</a>
+              </li>
+              <li class=" nav-link">
+                <a href="">Logout</a>
+              </li>
+              <li class=" nav-link"></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
