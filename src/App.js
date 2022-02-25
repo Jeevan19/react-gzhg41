f356353e2ed61f272b107781dd41f6c553c44732
Jeevan19 <jeevanmca.ppm@gmail.com>
2022-02-25 08:04:35 +0530

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import './style.css';
 import Header from './Components/Header';
 import Event from './Components/Event';
+import './style.css';
 
 export default function App() {
   return (
-    <div>
+    <React.Fragment>
       <Event />
-    </div>
+    </React.Fragment>
   );
 }
